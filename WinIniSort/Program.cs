089ac8d2b775f1d24fcdfcbc676754a20a086404
FastSlow <vladislavpopov27@gmail.com>
2020-11-17 18:17:42 +0500

    Добавьте файлы проекта.

---
+++ WinIniSort/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace WinIniSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Section> sections = ReadINI(@"C:\Users\vladi\Downloads\Пример_исходного_файла.ini");
+            sections.Sort();
+            WriteINI(@"C:\Users\vladi\Downloads\Пример_исходного_файла2.ini",sections);
+        }
+        static public void WriteINI(String path,List<Section> sections)
+        {
+            using(StreamWriter sw=new StreamWriter(path))
+            {
+                foreach (Section section in sections)
+                {
+                    sw.WriteLine();
+                    sw.WriteLine(section.Name);
+                    for (int i = 0; i < section.Count(); i++)
+                    {
+                        sw.WriteLine(section[i]);
+                    }
+                }
+            }
+        }
+        static public List<Section> ReadINI(String path)
+        {
+            List<Section> sections = new List<Section>();
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                Regex Sregex = new Regex(@"^\[\w*\]$");
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (Sregex.IsMatch(line))
+                    {
+                        sections.Add(new Section(line));
+                    }
+                    else
+                    {
+                        Regex Kregex = new Regex(@"\w*=\w*");
+                        if (Kregex.IsMatch(line))
+                        {
+                            String[] vs = line.Split('=');
+                            if (vs.Length>2)
+                            {
+                                Console.WriteLine("Нарушена структура файла. Нажмите любую клавишу, чтобы продолжить...");
+                                Console.ReadKey();
+                            }
+                            try
+                            {
+                                sections[sections.Count - 1].AddValue(vs[0], vs[1]);
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e.Message);
+                                Console.WriteLine("Нарушена структура файла. Нажмите любую клавишу, чтобы продолжить...");
+                                Console.ReadKey();
+                            }
+                        }
+                        else
+                        {
+                            if (line!=""&&line[0]!=';')
+                            {
+                                Console.WriteLine("Нарушена структура файла. Нажмите любую клавишу, чтобы продолжить...");
+                                Console.ReadKey();
+                            }
+                        }
+                    }
+                }
+            }
+            return sections;
+        }
+    }
+}
---
+++ WinIniSort/Section.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace WinIniSort
+{
+    class Section : IComparable<Section>
+    {
+        String name;
+        SortedList<String,String> keyValues = new SortedList<string, string>();
+        String KeyEx = @"[\!\@\#\$\%\^\&\*]"; //Символы которые не должны входить в ключ
+        String ValueEx = @"[\!\@\#\$\%\^\&\*]"; //Символы которые не должны входить в значение
+        public String Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public String this[int index]
+        {
+            get
+            {
+                return keyValues.Keys[index] + "=" + keyValues.Values[index];
+            }
+        }
+        public int Count()
+        {
+            return keyValues.Count;
+        }
+        public Section(String name)
+        {
+            this.name = name;
+        }
+        public void AddValue(String key,String value)
+        {
+            ExceptionKey(key);
+            ExceptionValue(value);
+            keyValues.Add(key, value);
+        }
+        public void ExceptionKey(String key)
+        {
+            Regex Kregex = new Regex(KeyEx);
+            if (Kregex.IsMatch(key))  //проверяем нет ли запрещенных символов
+            {
+                throw new Exception("Ключ неверного формата "+key);
+            }
+            else
+            {
+                Kregex = new Regex(@"[\[\]]");
+                if (Kregex.IsMatch(key))    //проверяем есть ли [ и ]
+                {
+                    Kregex = new Regex(@"^\w*[\[\]]\w*\[\]$");
+                    if (Kregex.IsMatch(key))    //проверяем есть ли формат строки типа asdw[eqw[]
+                    {
+                        throw new Exception("Ключ неверного формата " + key);
+                    }
+                    else
+                    {
+                        Kregex = new Regex(@"\[\]$");
+                        if (!Kregex.IsMatch(key))   //проверяем [ и ] стоят в конце, если нет, то формат ключа неверный
+                        {
+                            throw new Exception("Ключ неверного формата " + key);
+                        }
+                    }
+
+                }
+            }
+        }
+        public void ExceptionValue(String value)
+        {
+            String[] vs = value.Split(';');
+            String[] values = vs[0].Split(", ");
+            foreach (String item in values)
+            {
+                Regex Vregex = new Regex(ValueEx);
+                if (Vregex.IsMatch(item))  //проверяем нет ли запрещенных символов
+                {
+                    throw new Exception("Значение неверного формата " + value);
+                }
+            }
+        }
+        public int CompareTo(Section s)
+        {
+            return this.name.CompareTo(s.name);
+        }
+    }
+}
